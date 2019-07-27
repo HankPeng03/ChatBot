41ecb36eba7baf982a0c11b3c2ed4ace81e719fd
hao.peng <penghao189811@163.com>
2019-07-27 22:04:24 +0800

    #

--- main.py
+++ main.py
@@ -15,65 +15,8 @@ def my_encoder(hidden_size, num_layers, embedding_matrix, input_data, input_data
                       dtype=tf.float32)
     return encoder_output, encoder_state
 
-
-
-if __name__ == '__main__':
-    # 文件读取
-    with open("Temp/data.json", "r") as f:
-        data = json.load(f)
-    fromids = data["fromids"]
-    toids = data["toids"]
-    del data
-
-    with open("Temp/word_dict.json", "r") as f:
-        all_dict = json.load(f)
-
-    word2id = {w: id for id, w in enumerate(all_dict)}
-    id2word = {id: w for id, w in enumerate(all_dict)}
-    # 构建词向量矩阵
-    from gensim.models import Word2Vec
-    model = Word2Vec.load("Temp/word2vec.model")
-
-    import numpy as np
-    vocab_size = len(all_dict)
-    corpus_size = len(fromids)
-    emb_size = 100
-    embedding_matrix = np.zeros((vocab_size, emb_size), dtype=np.float32)
-    tmp = np.diag([1]*emb_size)  # 对于出现在词典中，但是没有出现在词向量中的词汇，使用One-hot代替
-    k = 0
-    for i in range(vocab_size):
-        try:
-            embedding_matrix[i] = model.wv[str(i)]
-        except:
-            embedding_matrix[i] = tmp[k]
-            k += 1
-    # 统计id向量的长度，并统一长度
-    from_length = [len(i) for i in fromids]
-    m = max(from_length)
-    source = [i + [word2id["_PAD"]]*(m-len(i)) for i in fromids]
-
-    to_length = [len(i) for i in toids]
-    m = max(to_length)
-    target = [i + [word2id['_PAD']]*(m-len(i)) for i in toids]
-
-    max_output_sequence_length = max(max(from_length), max(to_length))
-    # 定义Tensor
-
-    num_layers = 2
-    hidden_size = 100
-    learning_rate = 0.001
-    batch_size = 64
-
-    ## 设置占位符
-    input_data = tf.placeholder(dtype=tf.int32, shape=[corpus_size, None], name="source")  # 输入
-    output_data = tf.placeholder(dtype=tf.int32, shape=[corpus_size, None], name="target")  # 输出
-    input_data_length = tf.placeholder(dtype=tf.int32, shape=[corpus_size], name="source_sequence_length")  # 输入句子的长度
-    output_data_length = tf.placeholder(dtype=tf.int32, shape=[corpus_size], name="target_sequence_length")  # 输出句子的长度
-    emb_matrix = tf.constant(embedding_matrix, name="embedding_matrix", dtype=tf.float32)  # 词向量矩阵
-
-    # Encoder
-    encoder_output, encoder_state = my_encoder(hidden_size, num_layers, embedding_matrix, input_data, input_data_length)
-
+def my_decoder(output_data, corpus_size, word2id, embedding_matrix, hidden_size,num_layers,
+               vocab_size, output_sequence_length, max_output_sequence_length, encoder_output):
     # Decoder
     ## 添加_BOS
     ending = tf.strided_slice(output_data, begin=[0,0], end=[corpus_size, -1], strides=[1,1])
@@ -109,7 +52,7 @@ if __name__ == '__main__':
                                multiples=[corpus_size], name="start_tokens")
         inference_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(embedding=emb_matrix,
                                                  start_tokens=start_tokens,
-                                                 end_tokens=word2id["_EOS"])
+                                                 end_token=word2id["_EOS"])
         # Basic Decoder
         inference_decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cells,
                                                             helper=inference_helper,
@@ -118,13 +61,86 @@ if __name__ == '__main__':
         # Dynamic Decoder
         inference_final_output, inference_final_state, inference_sequence_length = tf.contrib.seq2seq.dynamic_decode(decoder=inference_decoder, imput_finished=True,
                                           naximun_iterations=max_output_sequence_length)
+    return training_final_output, training_final_state, inference_final_output, inference_final_state
+
+if __name__ == '__main__':
+    # 文件读取
+    with open("Temp/data.json", "r") as f:
+        data = json.load(f)
+    fromids = data["fromids"]
+    toids = data["toids"]
+    del data
+
+    with open("Temp/word_dict.json", "r") as f:
+        all_dict = json.load(f)
+
+    word2id = {w: id for id, w in enumerate(all_dict)}
+    id2word = {id: w for id, w in enumerate(all_dict)}
+    # 构建词向量矩阵
+    from gensim.models import Word2Vec
+    model = Word2Vec.load("Temp/word2vec.model")
+
+    import numpy as np
+    vocab_size = len(all_dict)
+    corpus_size = len(fromids)
+    emb_size = 100
+    embedding_matrix = np.zeros((vocab_size, emb_size), dtype=np.float32)
+    tmp = np.diag([1]*emb_size)  # 对于出现在词典中，但是没有出现在词向量中的词汇，使用One-hot代替
+    k = 0
+    for i in range(vocab_size):
+        try:
+            embedding_matrix[i] = model.wv[str(i)]
+        except:
+            embedding_matrix[i] = tmp[k]
+            k += 1
+    # 统计id向量的长度，并统一长度
+    input_sequence_length = [len(i) for i in fromids]
+    max_input_sequence_length = max(input_sequence_length)
+    source = [i + [word2id["_PAD"]]*(max_input_sequence_length-len(i)) for i in fromids]
+
+    output_sequence_length = [len(i) for i in toids]
+    max_output_sequence_length = max(output_sequence_length)
+    target = [i + [word2id['_PAD']]*(max_output_sequence_length-len(i)) for i in toids]
+    # 定义Tensor
+
+    num_layers = 2
+    hidden_size = 100
+    learning_rate = 0.001
+    batch_size = 64
+
+    ## 设置占位符
+    input_data = tf.placeholder(dtype=tf.int32, shape=[corpus_size, None], name="source")  # 输入
+    output_data = tf.placeholder(dtype=tf.int32, shape=[corpus_size, None], name="target")  # 输出
+    input_data_length = tf.placeholder(dtype=tf.int32, shape=[corpus_size], name="source_sequence_length")  # 输入句子的长度
+    output_data_length = tf.placeholder(dtype=tf.int32, shape=[corpus_size], name="target_sequence_length")  # 输出句子的长度
+    emb_matrix = tf.constant(embedding_matrix, name="embedding_matrix", dtype=tf.float32)  # 词向量矩阵
+
+    # Encoder
+    encoder_output, encoder_state = my_encoder(hidden_size, num_layers, embedding_matrix, input_data, input_data_length)
+
+    # Decoder
+    training_final_output, training_final_state, inference_final_output, inference_final_state = \
+        my_decoder(output_data, corpus_size, word2id, embedding_matrix, hidden_size, num_layers,
+               vocab_size, output_sequence_length, max_output_sequence_length, encoder_output)
+
+
 
 
     # Encoder-Decoder Model（Seq2Seq Model）
 
     ##  Loss Function
-
+    training_logits = tf.identity(input=training_final_output.rnn_output, name="training_logits")
+    inference_logits = tf.identity(input=inference_final_output.sample_id, name="inference_logits")
+    mask = tf.sequence_mask(lengths=output_sequence_length, maxlen=max_output_sequence_length,
+                            name="mask", dtype=tf.float32)
+    cost = tf.contrib.seq2seq.sequence_loss(logits=training_logits, targets=output_data,
+                                     weights=mask)
     ## Optimizer （自适应优化器）
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
     ## 梯度剪枝（防止梯度爆炸或梯度消失）
+    gradients = optimizer.compute_gradients(cost)
+    clip_gradients = [(tf.clip_by_value(t=grad, clip_value_max=5.0, clip_value_min=-5.0), var)
+                      for grad, var in gradients if grad is not None]
+    train_op = train_op = optimizer.apply_gradients(clip_gradients)
 
